c15a2b05669c7349d5f757fa75a88215475151ec
abdallamfathy <abdallamfathy@gmail.com>
2022-08-23 13:20:31 +0200

    Get random words form the array

--- main.js
+++ main.js
@@ -26,3 +26,32 @@ lettersArray.forEach(letter =>{
     lettersContainer.appendChild(letterSpan);
     
 })
+
+// Object of words + categories
+
+const words = {
+    programming : ["php","js","react","node","c","go","ruby"],
+    people: ["hitler","bob marley","elon musk","haredy","rashedy"],
+    movies: ["focus","home alone","parasite","inception","prestige"],
+    countries: ["palestine","syria","egypt","qatar","jordon"],
+}
+
+// Get keys of object
+let allKeys = Object.keys(words);
+// Make random number
+let randomNumber = Math.floor(Math.random() * allKeys.length);
+
+// Category
+let randomName = allKeys[randomNumber];
+
+// Category words
+let randomValue = words[randomName];
+
+// Random number depend on words
+let randomValueNumber = Math.floor(Math.random() * randomValue.length);
+
+// Chosen word
+let randomValueName = randomValue[randomValueNumber];
+
+// Create span
+document.querySelector(".game-info .category span").innerHTML = randomName + " " + ;
